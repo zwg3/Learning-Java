13a791705c0074447d0ad4430c1de923ef243055
zwg3333 <60962298+zwg3333@users.noreply.github.com>
2024-04-11 10:42:16 +0300

    Added Lab2

---
+++ src/lab2/Student.java
@@ -0,0 +1,42 @@
+//TASK 2
+package lab2;
+
+public class Student {
+    String name;
+    Integer age;
+    Integer mathScore;
+    Integer chemistryScore;
+    Integer literatureScore;
+
+    public Student(String name, Integer age, Integer mathScore, Integer chemistryScore, Integer literatureScore) {
+        this.name = name;
+        this.age = age;
+        this.mathScore = mathScore;
+        this.chemistryScore = chemistryScore;
+        this.literatureScore = literatureScore;
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name +
+                ", age=" + age +
+                ", mathScore=" + mathScore +
+                ", chemistryScore=" + chemistryScore +
+                ", literatureScore=" + literatureScore
+                ;
+    }
+}
+
+
+
+
+
