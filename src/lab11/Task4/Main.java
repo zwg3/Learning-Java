81e534b767cb75c46688ea434ba2d5104b62ee1b
zwg3333 <60962298+zwg3333@users.noreply.github.com>
2024-05-15 10:33:44 +0300

    L11

--- src/lab11/Task4/Main.java
+++ src/lab11/Task4/Main.java
@@ -1,7 +1,47 @@
 package lab11.Task4;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ThreadCaller.nameCall();
+    private static final Object lock = new Object();
+    private static boolean threadTurn = true;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                synchronized (lock) {
+                    try {
+                        while (!threadTurn) {
+                            lock.wait();
+                        }
+                        System.out.println(Thread.currentThread().getName());
+                        threadTurn = false;
+                        lock.notify();
+                    }
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            while (true) {
+                synchronized (lock) {
+                    try {
+                        while (threadTurn) {
+                            lock.wait();
+                        }
+                        System.out.println(Thread.currentThread().getName());
+                        threadTurn = true;
+                        lock.notify();
+                    }
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
     }
 }
